600e2660f65be1acf77096029f6fb3cf82d44e9a
Captain201203 <160829342+Captain201203@users.noreply.github.com>
2026-01-27 16:43:48 +0700

    Đổi đường dẫn api để deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 // Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/HomeWorkManager';
 
 // Connect to MongoDB
